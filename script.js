76eb7c4629635bea8266a0a0c6a7e460fdf7c512
jeffreyzschottche <jeffreyzschot@gmail.com>
2022-08-18 16:40:59 +0200

    finshed libary

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let addBookButton = document.querySelector('#addNewBook');
+let bookGrid = document.querySelector('.books');
 
+// dummy book
 let bookArray = [
     {
         title: "A hitchhickers guide to the galaxy",
@@ -12,55 +14,142 @@ let bookArray = [
 
 
 
+
+
 // constructor maken voor een boek
 // pushen naar array als object
-function Book(title, author, pages,){
-   this.title = title;
-   this.author = author;
-   this.pages = pages;
+function Book(title, author, pages, ) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 // prototype voor read value boolean
-Book.prototype.hasRead = function(){
-    if(document.querySelector('select[name="readit"]').value === 'Yes'){
+Book.prototype.hasRead = function () {
+    if (document.querySelector('select[name="readit"]').value === 'Yes') {
         return true;
-    }else{
+    } else {
         return false;
     }
 }
 
-
-
 addBookButton.addEventListener('click', () => {
+    // get input values
     let bookTitle = document.querySelector('input[name="booktitle"]');
     let bookAuthor = document.querySelector('input[name="author"]');
     let bookPages = document.querySelector('input[name="pages"]');
     let bookURL = document.querySelector('input[name="imageURL"]');
-    const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
-    if(bookURL.value.length > 0){
-        book.url = bookURL.value;
+
+    if (bookTitle.value < 1 || bookAuthor.value < 1 || bookPages.value < 1) {
+        alert('please fill in information about the book');
+        return;
+    } else {
+        // check if book has been read and if their added a URL 
+        const book = new Book(bookTitle.value, bookAuthor.value, bookPages.value);
+        if (bookURL.value.length > 0) {
+            book.url = bookURL.value;
+        }
+        if (book.hasRead() == true) {
+            book.readBool = true;
+        } else {
+            book.readBool = false;
+        }
+
+        // bookArray.push(book);
+
+        // get & set localstorage bookArr
+        let localBooks = JSON.parse(localStorage.getItem('books'));
+        localBooks.push(book);
+        localStorage.setItem('books', JSON.stringify(localBooks));
+
+
+        // hide modal after submit
+        let modal = document.querySelector('#addBookModal');
+        modal.style.display = 'none';
+        let modal2 = document.querySelector('.modal-backdrop');
+        modal2.style.display = 'none';
+
+        // call function to load all books
+        renderBooks();
+        window.location.reload();
     }
-    if(book.hasRead() == true){
-        book.readBool = true;
-    }else{
-        book.readBool = false;
-    }
-    // console.log(book);
-    bookArray.push(book);
-    // console.log(bookArray);
-    renderBooks();
-    let modal = document.querySelector('#addBookModal');
-    modal.style.display = 'none';
 })
 
 
 
 
-// op pagina laden boeken renderen in grid
-// bij nieuw toegevoegde ook renderen / sturen naar grid
 
-function renderBooks(){
-    bookArray.forEach((book) => {
-        alert(book.title);
-    })
+function renderBooks() {
+    // GET array and empty bookgrid so books arent doubles
+    let localBooks = JSON.parse(localStorage.getItem('books'));
+    bookGrid.innerHTML = '';
+
+
+    localBooks.forEach(book => {
+
+        // create div element
+        let divBook = document.createElement('div');
+        divBook.classList.add('bookParent');
+        let titleBook = document.createElement('h1');
+        let authorBook = document.createElement('h2');
+        let pagesBook = document.createElement('p');
+        let imgBook = document.createElement('img');
+        let readBook = document.createElement('select');
+        let deleteBook = document.createElement('button');
+        deleteBook.classList.add('btn');
+        deleteBook.classList.add('btn-danger');
+        deleteBook.onclick = deleteBookFromLS;
+        deleteBook.innerHTML = 'Delete Book';
+        let readBookOption = document.createElement('option');
+        let readBookOption2 = document.createElement('option');
+        readBookOption.text = 'Read the book';
+        readBookOption2.text = 'Did not read the book';
+
+        titleBook.textContent = book.title;
+        authorBook.textContent = book.author;
+        pagesBook.textContent = `Pages : ${book.pages}`;
+
+        // make book value read or not read
+        if (book.readBool == true) {
+            readBook.add(readBookOption);
+            readBook.add(readBookOption2);
+        } else {
+            readBook.add(readBookOption2);
+            readBook.add(readBookOption);
+        }
+
+        // check if book starts with https:// oor with www.
+        let regex = /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+        if (!regex.test(book.url)) {
+            // if not dummy book
+            imgBook.src = 'http://smartmobilestudio.com/wp-content/uploads/2012/06/leather-book-preview.png';
+        } else {
+            imgBook.src = book.url;
+        }
+
+        divBook.append(titleBook, authorBook, imgBook, pagesBook, readBook, deleteBook);
+        bookGrid.append(divBook);
+
+    });
+}
+
+
+function deleteBookFromLS() {
+    // get title from book trough button
+    let parentBook = this.parentNode.querySelector("h1").innerHTML;
+
+    // search ls array for parentBook, delete from array, place array back, reload page.
+    let localBooks = JSON.parse(localStorage.getItem('books'));
+
+    // find index of book in array
+    const indexOfObject = localBooks.findIndex(book => {
+        return book.title === parentBook;
+      });
+
+    // delete from array and set ls back
+    localBooks.splice(indexOfObject, 1);
+    localStorage.setItem('books', JSON.stringify(localBooks));
+
+    // reload window to show ls
+    window.location.reload();
 }
